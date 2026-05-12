9b474153fa051e150dbb181e2d9077d2accd6006
Aluno <e023aluno@outlook.com>
2026-05-12 20:31:16 -0300

    mini projeto vetor

---
+++ forEach.js
@@ -0,0 +1,7 @@
+const numeros = [1,2,3,4,5]
+
+function dobro(valor){
+    return console.log(valor * 2)
+}
+
+numeros.forEach(dobro)
---
+++ objeto.js
@@ -0,0 +1,16 @@
+let nome = "João"
+let idade = 25
+let curso = "Engenharia"
+
+let nome2 = "Maria"
+let idade2 = 22
+let curso2 = "Computação"
+// OBJETO LITERAL     -     SEM CLASSE
+const pessoa1 = {
+    //PROPRIEDADES
+    //CHAVE              VALOR
+    nome:               "João",
+    idade:                25,
+    curso:           "Engenharia"
+}
+console.log(pessoa1)
