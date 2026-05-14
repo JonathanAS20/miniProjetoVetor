d38301d1727ddfd3c5b1882e2dfa69171327a7ba
Jonathan Almeida <jhow572126@gmail.com>
2026-05-13 21:52:23 -0300

    projeto vetor finalizado

--- app.js
+++ app.js
@@ -30,18 +30,64 @@ function verHamburgueres(){
         let hamburguer = hamburgueria[cont]
         console.log(`${cont + 1} Nome: ${hamburguer.nome}, Preço: ${hamburguer.preco}`)
     }
+    //for(let i = 0; i <hamburgueria.length; i++){
+    //    hamburgueria.forEach((hamburguer) => {
+    //    console.log(`${i + 1} Nome: ${hamburguer.nome} Preço R$: ${hamburguer.preco}`)
+    //    })
+    //}
 }
 
+function atualizarHamburguer(){
+    verHamburgueres()
+    const index = parseInt(prompt('Escolha o hambúrguer para atualizar: ')) -1;
+    if(index < 0 || index>= hamburgueria.length){
+        console.log("Número inválido.")
+        return;
+    }
+    const novoNome = prompt("Novo nome: ");
+    const novoPreco = prompt("Novo preço: ");
+    hamburgueria[index] = {nome: novoNome, preco: novoPreco}
+    console.log("Atualizado com sucesso!")
+}
+function deletarHamburguer(){
+    verHamburgueres()
+    const index = parseInt(prompt('Escolha o hambúrguer para deletar: ')) -1;
+    if(index < 0 || index>= hamburgueria.length){
+        console.log("Número inválido.")
+        return;
+    }
+    hamburgueria.splice(index, 1);
+    console.log("Deletado com sucesso!");
+}
 
+function principal(){
+    let opcao
 
-
-
-
-
-
-
-
-
-adicionarHamburguer();
-adicionarHamburguer();
-verHamburgueres();
+    do{
+        exibirMenu()
+        opcao = parseInt(prompt("Digite uma opção: "));
+        switch(opcao){
+            case 1:
+                adicionarHamburguer();
+                break;
+            case 2:
+                verHamburgueres();
+                break;
+            case 3:
+                atualizarHamburguer();
+                verHamburgueres();
+                break;
+            case 4:
+                deletarHamburguer();
+                verHamburgueres();
+                break;
+            case 5:
+                console.log("Saindo...");
+                break;
+            default:
+                console.log("Opcão inválida!")
+                break;
+        }
+    }while(opcao !== 5);
+}
+principal()
